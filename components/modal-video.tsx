17df78287d6fa55765d6990ad916f2e114d881c9
rraj-official <119522203+rraj-official@users.noreply.github.com>
2023-07-30 20:36:48 +0530

    thumbnail update

--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -54,14 +54,7 @@ export default function ModalVideo({
                 </radialGradient>
                 <circle id="hero-ill-d" cx="384" cy="216" r="64" />
               </defs>
-              <g fill="none" fillRule="evenodd">
-                <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128" />
-                <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96" />
-                <g fillRule="nonzero">
-                  <use fill="#000" xlinkHref="#hero-ill-d" />
-                  <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
-                </g>
-              </g>
+            
             </svg>
           </div>
           <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => { setModalOpen(true) }}>
@@ -69,7 +62,8 @@ export default function ModalVideo({
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
             </svg>
-            <span className="ml-3">Watch the full video (30 sec)</span>
+            <span className="ml-3">
+              Watch the video to receive a welcome from AI</span>
           </button>
         </div>
       </div>
